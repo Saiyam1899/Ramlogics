84c5a1a4d683a9fac9f9b12c9abf79f1f1fa7fb0
Saiyam1899 <95686190+Saiyam1899@users.noreply.github.com>
2021-12-21 15:54:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <center>
+      <img src={process.env.PUBLIC_URL+"/images/popup.jpg"} className='image'/>
+    </center>
+    
+    </div>
+  );
+}
+
+export default App;
